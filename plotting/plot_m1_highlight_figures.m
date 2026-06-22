cb8dfb0d44bc96b3ebf463809b12edd95bb965ad
spyspy99a88b <8270952@qq.com>
2026-06-23 00:52:55 +0800

    Annotate misbehaved USVs in spatiotemporal figure

--- plotting/plot_m1_highlight_figures.m
+++ plotting/plot_m1_highlight_figures.m
@@ -89,6 +89,7 @@ function plot_spatiotemporal_trajectory(d, C, outDir)
             'FontWeight', 'bold', 'Color', timeColor, ...
             'HorizontalAlignment', 'left', 'VerticalAlignment', 'middle');
     end
+    annotate_misbehavior_event(ax, d);
 
     xlabel(ax, '$x$ (m)'); ylabel(ax, '$y$ (m)'); zlabel(ax, 'Time (s)');
     title(ax, 'Spatiotemporal evolution of the enclosing formation', ...
@@ -104,6 +105,25 @@ function plot_spatiotemporal_trajectory(d, C, outDir)
     cb.Label.String = 'Time (s)';
     clim(ax, [d.t(1) d.t(end)]);
     save_figure(fig, outDir, 'FigH1_Spatiotemporal_Trajectory');
+end
+
+function annotate_misbehavior_event(ax, d)
+    [~, k45] = min(abs(d.t-45));
+    active45 = find(d.validMask(k45,:));
+    xSnap = reshape(d.pAgent(k45,1,active45), [], 1);
+    ySnap = reshape(d.pAgent(k45,2,active45), [], 1);
+    xText = min(xSnap) - 58;
+    yText = max(ySnap) + 22;
+    zText = d.t(k45) + 4.5;
+    text(ax, xText, yText, zText, ...
+        {'$t=50$ s'; 'USV5, USV6 misbehaved'}, ...
+        'FontSize', 7.8, 'FontWeight', 'bold', ...
+        'Color', [0.72 0.08 0.10], ...
+        'BackgroundColor', [1.00 0.98 0.97], ...
+        'Margin', 1.8, ...
+        'HorizontalAlignment', 'left', ...
+        'VerticalAlignment', 'middle', ...
+        'HandleVisibility', 'off');
 end
 
 function draw_target_icon_3d(ax, x0, y0, z0, scale)
